73cb524a173e4d92806648764f660a6b45ee1534
tolgakrc <tolgakaraca.official@gmail.com>
2022-08-21 14:59:22 +0300

    BoostArea fixed2.

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Managers
+{
+    public class ScoreManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Private Variables
+
+        private int _score;
+        private GameObject _scoreGameObject;
+
+        #endregion
+
+        #endregion
+        
+    }
+}

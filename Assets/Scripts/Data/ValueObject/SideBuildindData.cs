e0060c338abbedf1bf5e90b3d08f3b9f64af7498
tolgakrc <tolgakaraca.official@gmail.com>
2022-08-26 13:42:37 +0300

    IdleLevelDatas added.

---
+++ Assets/Scripts/Data/ValueObject/SideBuildindData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Enums;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+
+namespace Data.ValueObject
+{
+    [Serializable]
+    public class SideBuildindData
+    {
+        [Title("Building State")] 
+        [EnumPaging] 
+        public BuildingState BuildingState = BuildingState.Uncompleted;
+        [HorizontalGroup("Game Data",75)]
+        [PreviewField(100)]
+        [HideLabel]
+        public GameObject Building;
+        [VerticalGroup("Game Data/Stats")]
+        [LabelWidth(100)]
+        public int SideBuildingScore;
+        [VerticalGroup("Game Data/Stats")]
+        [LabelWidth(100)]
+        public int CurrentScore;
+        [VerticalGroup("Game Data/Stats")]
+        [LabelWidth(100)] 
+        public Vector2 InstantitatePos;
+        [VerticalGroup("Game Data/Stats")]
+        [LabelWidth(100)]
+        public TextMeshPro BuildingScoreTMP;
+        [VerticalGroup("Game Data/Stats")]
+        [LabelWidth(100)] 
+        public Vector2 InstantitatePosTMP;
+    }
+}

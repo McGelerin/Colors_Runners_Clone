6f171270a76aa47ee11a1e79701a20883e0ccf1f
OnurHG <onurhangumus@gmail.com>
2022-08-29 18:17:34 +0300

    -Collectable and player animator has seperated

--- Assets/Scripts/Controllers/PlayerMeshController.cs
+++ Assets/Scripts/Controllers/PlayerMeshController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Data.UnityObject;
+using Data.ValueObject;
+using UnityEngine;
 
 namespace Controllers
 {
@@ -11,12 +13,28 @@ namespace Controllers
         [SerializeField] private SkinnedMeshRenderer skinnedMeshRenderer;
 
         #endregion
+        #region Private Variables
+        private ColorData _colorData;
 
         #endregion
+        #endregion
+        private void Awake()
+        {
+            _colorData = GetColorData();
+            ColorSet();
+        }
 
+        private ColorData GetColorData() => Resources.Load<CD_Color>("Data/CD_Color").colorData;
         public void ShowSkinnedMesh()
         {
             skinnedMeshRenderer.enabled = true;
+        }
+
+        public void ColorSet()
+        {
+            
+            skinnedMeshRenderer.material = _colorData.RainbowMaterial;
+        
         }
     }
 }
